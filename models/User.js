3c1b6b3d6c0858226f7d9bf4f80f1cc90116e937
Marrione Nguyen <mtiamsic@gmail.com>
2020-11-21 22:03:58 -0800

    update user js

--- models/User.js
+++ models/User.js
@@ -15,7 +15,7 @@ const UserSchema = new Schema({
     },
     thoughts: [{
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'Thought'
     }],
     friends: [{
         type: Schema.Types.ObjectId,

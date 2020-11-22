2c6c704180d690d8ce5224ea312839b1b799c3dc
Marrione Nguyen <mtiamsic@gmail.com>
2020-11-21 22:54:34 -0800

    test

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought, 
+    updateThought,
+    deleteThought,
+    addaReaction,
+    removeaReaction
+} = require('../../controllers/thought-controller')
+
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+router.route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router.route('/:thoughtId/reactions')
+    .post(addaReaction);
+
+router.route('/:thoughtId/reations/:reactionId')
+    .delete(removeaReaction);
+
+module.exports = router;
